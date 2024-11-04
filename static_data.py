7b431381c7b2543c23b861ddc38ca594e7afd06a
Naman Mishra <namanmishra1900@gmail.com>
2024-11-04 13:59:00 +0530

    updates static data

--- static_data.py
+++ static_data.py
@@ -2,63 +2,61 @@
 
 applications = {
     "github_copilot": r"githubcopilot",
-    "grammarly": r"grammarly",
     "leena": r"leena",
     "microsoft_copilot": r"copilot",
     "multion": r"multion",
     "notion": r"notion",
     "postman": r"postman",
-    "slack" : r"slack",
-    "jasper" : r"jasper",
+    "slack": r"slack",
+    "jasper": r"jasper",
 }
 
 llm_mapping = {
     "github_copilot": "GPT-3.5",
     "microsoft_copilot": "GPT-3.5",
-    "grammarly": "GPT-3.5",
     "leena": "GPT-3.5",
     "multion": "GPT-3.5",
     "notion": "GPT-3.5",
-    "postman" : "GPT-4",
-    "slack" : "Claude-3.5-sonnet",
-    "jasper" : "Claude-3.5-sonnet"
+    "postman": "GPT-4",
+    "slack": "Claude-3.5-sonnet",
+    "jasper": "Claude-3.5-sonnet",
 }
 
 risk_ratings = {
     "gpt-3.5": {
-        "overall_rating": 47,
+        "overall_rating": 77,
         "prompt_injection": 68,
-        "jailbreak": 52,
-        "bias": 22,
+        "jailbreak": 55,
+        "bias": 23,
         "hallucination": 49,
         "data_poisoning": 71,
-        "data_leakage": 37
+        "data_leakage": 29,
     },
     "gpt-4-mini": {
-        "overall_rating": 76,
+        "overall_rating": 63,
         "prompt_injection": 54,
-        "jailbreak": 58,
-        "bias": 52,
-        "hallucination": 77,
+        "jailbreak": 45,
+        "bias": 31,
+        "hallucination": 61,
         "data_poisoning": 46,
-        "data_leakage": 61
+        "data_leakage": 34,
     },
     "gpt-4": {
-        "overall_rating": 78,
-        "prompt_injection": 72,
-        "jailbreak": 64,
-        "bias": 27,
-        "hallucination": 68,
-        "data_poisoning": 59,
-        "data_leakage": 57
+        "overall_rating": 34,
+        "prompt_injection": 29,
+        "jailbreak": 38,
+        "bias": 15,
+        "hallucination": 47,
+        "data_poisoning": 32,
+        "data_leakage": 28,
     },
     "claude-3.5-sonnet": {
-        "overall_rating": 52,
-        "prompt_injection": 48,
-        "jailbreak": 53,
-        "bias": 47,
-        "hallucination": 51,
-        "data_poisoning": 55,
-        "data_leakage": 43
-    }
+        "overall_rating": 26,
+        "prompt_injection": 29,
+        "jailbreak": 25,
+        "bias": 16,
+        "hallucination": 33,
+        "data_poisoning": 22,
+        "data_leakage": 18,
+    },
 }
